e0957439f3b79a69faaf383029150c3a4656aca0
whdgus1323 <whdgus1323@gmail.com>
2025-12-03 22:14:13 +0900

    -

--- 2025-12-03/deep_5.py
+++ 2025-12-03/deep_5.py
@@ -8,13 +8,25 @@ Created on Wed Dec  3 13:51:09 2025
 import numpy as np
 import matplotlib.pyplot as plt
 
-x, y = np.meshgrid(
-    np.linspace(-2, 2, 3), 
-    np.linspace(-3, 3, 3)
-)
-z = np.exp(-x ** 2 -y ** 2)
+import tensorflow as tf
+from tensorflow import keras
+
+x = np.arange(0, 1000)
+y = np.arange(0, 1000)
+
+model = keras.Sequential([
+    keras.layers.Dense(8, activation='relu', input_shape=(1,)),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x, y, epochs=100, batch_size=32, verbose=1)
 
 
-print(x)
-print(y)
-print(z)
+model = keras.Sequential([
+    keras.layers.Dense(8, activation='relu', input_shape=(1,)),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x, y, epochs=100, batch_size=32, verbose=1)
